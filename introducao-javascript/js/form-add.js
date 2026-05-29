84d40e114a842c91bba2fd844b70cc9738cdd405
Bladimir Jose Villarroel Guevara <bladimir.dev.contato@gmail.com>
2026-05-28 21:47:17 -0300

    Adicionei os arquivos de js

---
+++ introducao-javascript/js/form-add.js
@@ -0,0 +1,6 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Fui clicado!");
+})
